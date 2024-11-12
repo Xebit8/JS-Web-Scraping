1dcad7aaa0651005e70c13407c92a8aad6c34bb7
Anton <52106716+Xebit8@users.noreply.github.com>
2024-11-12 21:23:20 +0300

    cut to 3 user-agents per parser

--- pracezarohem/get_pracezarohem.js
+++ pracezarohem/get_pracezarohem.js
@@ -7,21 +7,11 @@ const fs = require('fs').promises;
 
 const base_url = "https://www.pracezarohem.cz/nabidky/";
 
+// Alternating userAgents (Tiny measures to bypass the blocking)
 const userAgents = [
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 7_0_2) Gecko/20100101 Firefox/53",
-    "Mozilla/5.0 (compatible; MSIE 9.0; Windows; Windows NT 6.1; WOW64 Trident/5.0)",
-    "Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; en-US) Gecko/20100101 Firefox/62.3",
-    "Mozilla/5.0 (Windows; U; Windows NT 10.0; Win64; x64; en-US) AppleWebKit/537.32 (KHTML, like Gecko) Chrome/48.0.3931.209 Safari/536.4 Edge/13.26854",
-    "Mozilla/5.0 (compatible; MSIE 7.0; Windows NT 6.0;; en-US Trident/4.0)",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 7_8_2; en-US) Gecko/20100101 Firefox/65.2",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 11_8_5; like Mac OS X) AppleWebKit/537.24 (KHTML, like Gecko) Chrome/52.0.3825.262 Mobile Safari/535.2",
-    "Mozilla/5.0 (Linux; Linux i652 ) AppleWebKit/600.30 (KHTML, like Gecko) Chrome/54.0.2777.319 Safari/600",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 9_2_5; like Mac OS X) AppleWebKit/601.6 (KHTML, like Gecko) Chrome/50.0.3355.226 Mobile Safari/537.3",
-    "Mozilla/5.0 (Android; Android 5.0; SM-G400 Build/LRX22C) AppleWebKit/537.48 (KHTML, like Gecko) Chrome/51.0.1416.156 Mobile Safari/533.9",
 ];
 const ua = userAgents[Math.floor(Math.random() * userAgents.length)];
 const headers = {
